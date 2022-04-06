e5e60d28ce73167c9e89b862fc1f95876dd4924b
sufiaalmas <sufiaalmas4@gmail.com>
2022-04-06 09:36:58 +0000

    Adding route

--- aws.tf
+++ aws.tf
@@ -13,4 +13,11 @@ Name="sufia-gateway"
 }
 }
 
+resource "aws_route" "sufia-route" {
+        destination_cidr_block = "0.0.0.0/0"
+        gateway_id = "${aws_internet_gateway.sufia-gateway.id}"
+        route_table_id = "${aws_vpc.sufia-vpc.main_route_table_id}"
+	}
+
+
 
